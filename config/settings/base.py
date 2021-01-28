fee69b47ef4d0a2b38b99e4fc5cbed3a90fc1c18
MikaelSantilio <mikael.santilio@gmail.com>
2021-01-28 10:02:10 -0300

    removed simple jwt code

--- config/settings/base.py
+++ config/settings/base.py
@@ -307,45 +307,10 @@ CORS_URLS_REGEX = r'^.*$'
 # django-cors-whitelist - # https://github.com/adamchainz/django-cors-headers#cors_origin_whitelist
 CORS_ORIGIN_WHITELIST = env.list("CORS_ORIGIN_WHITELIST", default=[])
 
-# SIMPLE_JWT = {
-#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-#     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-#     # 'ROTATE_REFRESH_TOKENS': False,
-#     # 'BLACKLIST_AFTER_ROTATION': True,
-#     'UPDATE_LAST_LOGIN': False,
-
-#     # 'ALGORITHM': 'HS256',
-#     # 'SIGNING_KEY': settings.SECRET_KEY,
-#     # 'VERIFYING_KEY': None,
-#     # 'AUDIENCE': None,
-#     # 'ISSUER': None,
-
-#     'AUTH_HEADER_TYPES': ('Bearer',),
-#     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-#     # 'USER_ID_FIELD': 'id',
-#     # 'USER_ID_CLAIM': 'user_id',
-
-#     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-#     # 'TOKEN_TYPE_CLAIM': 'token_type',
-
-#     # 'JTI_CLAIM': 'jti',
-
-#     # 'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-#     # 'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-#     # 'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-# }
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': False,
-    'ALGORITHM': 'HS256',
-    # 'SIGNING_KEY': SECRET_KEY,
-    'VERIFYING_KEY': None,
     'AUTH_HEADER_TYPES': ('JWT',),
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
 }
