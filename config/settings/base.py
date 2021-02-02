91dc9bd978c5f4e0454fc866fb787cb7e3a97a05
MikaelSantilio <mikael.santilio@gmail.com>
2021-02-02 18:29:01 -0300

    test throttling

--- config/settings/base.py
+++ config/settings/base.py
@@ -294,7 +294,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.UserRateThrottle"
     ],
     "DEFAULT_THROTTLE_RATES": {
-        "anon": "100/day",
+        "anon": "0/day",
         "user": "1000/day"
     }
 }
